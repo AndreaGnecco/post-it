7a465e042af6f12cd3839a0f2429170487fbf087
andreagnecco <andreagnecco@gmail.com>
2018-01-23 21:54:03 -0200

    Segundo commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,38 @@
 var notas = [];
 
-function atualizarSecao() {
+function atualizarSecao(secao) {
     //criar uma variavel que vai guardar o html de todas as notas que devem aparecer na tela
 
-    var htmlNotas = document.createElement("form");
-    htmlNotas.setAttribute("class", "new-note");
-    form.innerHTML = `<h1></h1>
-                        <textarea></textarea>
-                        <button>Excluir Nota</button>`
-
+    var htmlNotas = "";
 
     //percorrer a lista de notas e criar o template de cada nota, criar o html de cada nota e colocar na variavel 
+    for (var i = 0; i < notas.length; i++) {
+        htmlNotas += '<form class="note" onclick="editarFormulario()">' +
+                        '<button class="note__control" type="button" onclick="removerNota(' + i + ', this.form.parentElement)">' +
+                            '<i class="fa fa-times" aria-hidden="true"></i>' +
+                        '</button>' +
+                        '<h1 class="note__title">' + notas[i].titulo + '</h1>' + 
+                         '<p class="note__body">' + notas[i].texto +
+                         '</p>' + 
+                    '</form>'
 
+    }
     //colocar o html de todo mundo dentro da secao
+    secao.innerHTML = htmlNotas;
+
+}
+
+function editarFormulario(i){
+    //pegar a nota e setar editando = true
+    notas[i].editando = true
+    
+    //chamar o atualiza tela
+    atualizarSecao(secao);
 
 
 }
 
-function adicionarNota(inputTitulo, textareaTexto, formulario) {
+function adicionarNota(inputTitulo, textareaTexto, formulario, secao) {
     //criar uma variavel nota
     var nota = {
         titulo: inputTitulo.value,
@@ -28,8 +43,18 @@ function adicionarNota(inputTitulo, textareaTexto, formulario) {
     notas.push(nota);
 
     //atualizar a secao de notas
-    atualizarSecao();
+    atualizarSecao(secao);
 
     //limpar o formulario
-    formulario.reset();
+    formulario.reset(secao);
+}
+
+function removerNota(i, secao){
+    //remover nota da lista de notas
+
+    notas.splice(i, 1)
+
+    //atualizar tela
+
+    atualizarSecao(secao)
 }
